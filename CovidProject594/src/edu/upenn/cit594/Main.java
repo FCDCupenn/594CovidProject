2a8945c45afe6e166468e0fca6e5a13e4fd0c1e2
sunx68 <sunxin@seas.upenn.edu>
2023-04-19 21:18:16 -0700

    Merge branch 'master' of https://github.com/FCDCupenn/594CovidProject.git

--- CovidProject594/src/edu/upenn/cit594/Main.java
+++ CovidProject594/src/edu/upenn/cit594/Main.java
@@ -16,7 +16,9 @@ import edu.upenn.cit594.datamanagement.PropertyReader;
 import edu.upenn.cit594.processor.CovidDataProcessor;
 import edu.upenn.cit594.processor.PopulationDataProcessor;
 import edu.upenn.cit594.processor.PropertyAnalyzer;
+import edu.upenn.cit594.ui.Userinterface;
 import edu.upenn.cit594.util.Covid;
+import edu.upenn.cit594.util.FileCreater;
 import edu.upenn.cit594.util.Population;
 import edu.upenn.cit594.util.Property;
 
@@ -51,9 +53,23 @@ public class Main {
 //			}
 //		} while (choice < 8);
 		
+		if (args.length > 4) {
+          System.err.println("incorrect number of arguments");
+          return;
+       }	
+		// construct the new file array input from the args
+		
+		String[] files = args;
+		
+		String[] finalFiles = FileCreater.createFiles(files);
+		for (int i = 0; i < finalFiles.length; i++) {
+			System.out.println(finalFiles[i]);
+		}
+		
 		// only for test.....
 		String[] filenames_1 = {"covid_data.json", "properties.csv", "population.csv", "log_1"};
 		String[] filenames_2 = {"covid_data.csv", "properties.csv", "population.csv", "log_2"};
+		String[] filenames_3 = {"", "", "", "log_1"};
 		
 		AlmightyReader reader_json = new AlmightyReader(filenames_1);
 		AlmightyReader reader_csv = new AlmightyReader(filenames_2);
@@ -69,5 +85,10 @@ public class Main {
 		cdp_json.getpartialOrFullVacPerCapita(date, partial));
 		System.out.println(
 		cdp_csv.getpartialOrFullVacPerCapita(date, partial));
+		
+		
+		Userinterface.printAvailableActionsOptions(filenames_3);
+		
+		
 	}
 }
--- CovidProject594/src/edu/upenn/cit594/ui/Userinterface.java
+++ CovidProject594/src/edu/upenn/cit594/ui/Userinterface.java
@@ -1,4 +1,139 @@
 package edu.upenn.cit594.ui;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Userinterface {
+	
+	
+
+	/**
+	 * this method will check if all input files are valid
+	 * @param filenames
+	 * @return return true if the file is valid
+	 */
+	public static boolean checkValidFileNames(String[] filenames){
+		
+		String [] prefix = {"--covid=", "--properties=", "--population=", "--log="};
+		// check format
+		String regex = "^--(?<name>.+?)=(?<value>.+)$";
+		Pattern p1 = Pattern.compile(regex);
+		for (String s : filenames) {
+			Matcher m1 = p1.matcher(s);
+			// if it matches
+			if (m1.find()) {
+				// check if the file existed 
+				File file = new File(s);
+				if(file.exists()) {
+					// if the file exists
+					// check the name argument
+					for (int i = 0; i< prefix.length; i++) {
+						if (s.startsWith(prefix[i])) {
+							// if the file contain the prefix, need to check if there is a duplicate
+							
+							// the index will always start at 0
+							int index = s.substring(prefix[i].length()-1).indexOf(prefix[i]);
+							// if there is a replicate, then index will not be -1
+							// so it will return false
+							if (index != -1) return false;
+						}
+					}
+									
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+	
+	/**this method will return a list of options
+	 * 
+	 * @param filenames [covid_data, properties_data, population_data, log_file]
+	 * @return
+	 */
+	private static List<Integer> getAvailableActionsOptions(String[] filenames) {
+		// assume all files are correct	
+		// population data will have option 2
+		// covid data will have 3, 7 only when population data exist
+		// property data will show 4, 5, and show 6 only when population data exist
+		List<Integer> options = new ArrayList<>();
+		// all input would have option 0
+		options.add(0);
+		options.add(1);
+		List<Boolean> checkOptions = new ArrayList<>();
+		
+		// construct a checkOptions pannel
+		for (int i = 0; i < filenames.length; i ++) {
+			// the length of the file is not empty, means there is a file
+			if (filenames[i].length() != 0) {
+				checkOptions.add(true);
+			}
+			else checkOptions.add(false);
+		}
+		// check if population data exist
+		if (checkOptions.get(2)) {
+			options.add(2);
+			// if covid data exist
+			if (checkOptions.get(0)) {
+				options.add(3);
+				options.add(7);
+			}
+			// if property data exist
+			if (checkOptions.get(1)) {
+				options.add(4);
+				options.add(5);
+				options.add(6);
+			}
+			Collections.sort(options);
+			return options;
+			
+		}
+		else {
+			// only covid exist
+			if (checkOptions.get(0)) {
+				options.add(7);
+			}
+			// if property data exist
+			if (checkOptions.get(1)) {
+				options.add(4);
+				options.add(5);
+
+			}
+		}
+		Collections.sort(options);
+		return options;
+		
+	}
+	
+	/**
+	 * this will print out the available actions
+	 * @param filenames
+	 */
+	
+	public static void printAvailableActionsOptions(String[] filenames){
+		List<Integer> options = new ArrayList<>();
+		options = getAvailableActionsOptions(filenames);
+		System.out.println("BEGIN OUTPUT");
+		for (int i = 0; i < options.size(); i++) {
+			System.out.println(options.get(i));
+		}
+		System.out.println("END OUTPUT");
+		
+	}
+	
+	
+	
+	
+	
 }
